99a28f1f9e97e8039e98122861b4f4f34d79fa87
rafaelserpaa <rafaelserpa01@gmail.com>
2024-10-20 19:41:53 -0300

    client test NEED FIX

---
+++ cypress/e2e/clientes.cy.js
@@ -0,0 +1,44 @@
+describe('clientes', () => {
+  it('Cadastro com sucesso', () => {
+    cy.visit('/');
+    cy.get('.register').click();
+    cy.get('#username').type('rafael');
+    cy.get('#email').type('rafaelserpa01@gmail.com');
+    cy.get('#password').type('123');
+    cy.get('#password2').type('123');
+    cy.get('.btnbtn-primary').click();
+    // cy.get('.alert').invoke('text').should('have.string', "Your account has been created");
+  })
+  it('login com sucesso', () => {
+    cy.visit('login/');
+    cy.get('#username').type('rafael');
+    cy.get('#password').type('123');
+    cy.get('.btnbtn-primary').click();
+    // cy.get('.account > span').invoke('text').should('have.string', "Test User")
+    cy.visit('');
+    cy.get('.cliente').click();
+    cy.get('.add-client').click();
+    cy.get('#name').type('rafael');
+    cy.get('#cell').type('81995750921');
+    cy.get('#birthdate').type('2007-05-22')
+    cy.get('#cpf').type('13387878451');
+    cy.get('#cep').type('53439-360');
+    cy.get('#adress').type('Rua rui barbosa,832');
+
+    
+    cy.get('#states') // Comando pai
+      .select('Pernambuco'); // Comando filho encadeado
+    
+
+  
+  
+
+    cy.get('#city').type('paulista');
+    cy.get('#neighborhood').type('janga');
+    cy.get('#role').type('estudante');
+    cy.get('.submit-button').click();
+  })
+ 
+
+
+})
