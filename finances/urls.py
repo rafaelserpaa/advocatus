b9d591a98ea91fcd15c586e8970c175286e941ac
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2024-11-08 11:45:39 -0300

    change urls.py

--- finances/urls.py
+++ finances/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.register_finances, name="register_finances"),
-    path('edit-expense/<int:expense_id>/', views.edit_expense, name='edit_expense'),
-    path('delete-expense/<int:expense_id>/', views.delete_expense, name='delete_expense'),
-    path("reports/", views.reports, name="reports")
+    path("", views.finances, name="finance_home"),  # Página principal de finanças
+    path("finance_register/", views.register_finance, name="finance_register"),  # Página para cadastrar despesas
+    path("finances_edit/<int:id>/", views.edit_finance, name="finance_edit"),  # Página para editar uma despesa
+    path("finances_delete/<int:id>/", views.delete_finance, name="finance_delete"),  # Página para deletar uma despesa
+    path("reports/", views.reports, name="reports")  # Página de relatórios
 ]
